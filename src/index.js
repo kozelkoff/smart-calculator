47383a615fa1c38096a88a90ca9ad4bbe0d247fb
Pavel Kozelkov <pavelalexandrovich841@gmail.com>
2018-03-09 16:44:30 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,26 +1,57 @@
+var counter=1;
 class SmartCalculator {
   constructor(initialValue) {
     // your implementation
+      this.value = [];
+      this.value.push(initialValue);
+      counter=1;
+      return this;
   }
 
   add(number) {
     // your implementation
+      this.value.push('+', number);
+      counter=1;
+      return this;
   }
   
   subtract(number) {
     // your implementation
+      this.value.push('-', number);
+      counter=1;
+      return this;
   }
 
   multiply(number) {
     // your implementation
+      this.value.push('*', number);
+      counter=1;
+      return this;
   }
 
   devide(number) {
     // your implementation
+      this.value.push('/', number);
+      counter=1;
+      return this;
   }
 
   pow(number) {
     // your implementation
+      this.value[this.value.length - counter] = 'Math.pow(' + this.value[this.value.length - counter] + ', ';
+      this.value.splice(this.value.length - counter + 1);
+      this.value.push(number);
+      for(let i = 0; i < counter; i++) {
+          this.value.push(')');
+      }
+      counter++;
+
+      return this;
+  }
+
+
+  valueOf(){
+  return eval(this.value.join(''));
   }
 }
 
